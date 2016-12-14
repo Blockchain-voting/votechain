119e784bfc3ffba7d56d43bfabd185c66f5bb93b
Philip Winchester <philipwinc@gmail.com>
2016-12-14 10:37:53 -0500

    added debugging lines

--- Vote.py
+++ Vote.py
@@ -1,5 +1,5 @@
 class Vote:
     def __init__(self, e_id, choice, public_key):
         self.id = int(e_id)
-        self.choice = choice
+        self.choice = int(choice)
         self.public_key = public_key
--- server.py
+++ server.py
@@ -51,6 +51,7 @@ def vote():
     # create Vote item for vote dictionary purely b/c I find it easier to work with objects that I can manipulate how I like
     # might remove this if it isn't repeated too much
     vote = Vote(vote_dict.get('election'), vote_dict.get('options'), vote_dict.get('userPublicKey'))
+    print len(Elections), vote.id
     election = Elections[vote.id]
     # print 'vote request:', vote, election.name
     vote_hash = election.vote(vote)
